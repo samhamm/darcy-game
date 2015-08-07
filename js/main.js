85c1358b100590aa21b6d5c767686b0000141753
samhamm <samhamm@gmail.com>
2015-08-07 10:33:58 -0700

    first commit: inclusion of DOM manipulation

---
+++ js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var guess, answer, message;
+
+// Generate random number between 1 and 100
+answer = Math.floor(Math.random() * 100 + 1);
+console.log(answer);
+
+guess = prompt("I'm thinking of a number between 1 and 100... what is it?");
+
+if (guess == answer) {
+  message = "Are you a wizard? You got it right!";
+} else {
+  message = "Oh well. You only had a 1% chance anyway. Don't cry.";
+}
+
+var darcySays = document.getElementById('darcy-says');
+darcySays.innerHTML = message + '<p><img src="img/darcy-sam.jpg"></p>';
